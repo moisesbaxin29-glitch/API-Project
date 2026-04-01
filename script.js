6bc4bb6c567cb4d0741f6d6d4d2eb15d403c26ba
moisesbaxin29-glitch <moisesbaxin29@gmail.com>
2026-04-01 09:45:52 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
-if (Math)
-
-
-
-
-let math = document.querySelector(".Math")
-
+let data;
+let button = document.querySelector(".button");
+let output = document.querySelector(".output");
+let dropdown = document.querySelector(".dropdown");
+let choice;
 fetch("https://yesno.wtf/api")
   .then(function(response) {
     return response.json();
   })
   .then(function(data) {
-  
-console.log(data)
+
+console.log("data")
+document.querySelector("form").addEventListener('submit', function(e){
+  e.preventDefault();
+  console.log("clicked");
+choice = dropdown.value;
+
 
 output.innerHTML = data.answer;
-});
+  });
+
+  });
